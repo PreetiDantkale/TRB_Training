e6340f2f6836b4eabb8b6a011366821874ef28ae
PreetiDantkale <dantkalepreeti29@gmail.com>
2020-07-09 19:43:32 +0530

    Added index operation for user

---
+++ app/concepts/user/operation/index.rb
@@ -0,0 +1,25 @@
+module User::Operation
+  class Index < Trailblazer::Operation
+
+    step :valid_user
+    step :list
+    fail :set_error_response
+    pass :set_response_data
+
+    def valid_user(ctx, **)
+      true
+    end
+
+    def list(ctx, **)
+      ctx[:users] = User.all
+    end
+
+    def set_error_response(ctx, **)
+      ctx[:errors] = 'User Invalid...'
+    end
+
+    def set_response_data(ctx, **)
+      ctx[:response] = 'Users loaded successfully..'
+    end
+  end
+end
